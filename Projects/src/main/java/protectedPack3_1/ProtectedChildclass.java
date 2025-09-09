f82c45c50f413970dea256ed9091c7c63befc9bd
reshm <reshm@ReshmaTS>
2025-09-08 20:20:39 -0400

    Commit hello world

--- Projects/src/main/java/protectedPack3_1/ProtectedChildclass.java
+++ Projects/src/main/java/protectedPack3_1/ProtectedChildclass.java
@@ -1,6 +1,7 @@
 package protectedPack3_1;
+import protectedPack3.ProtectedPrgm3rdWay;
 
-public class ProtectedChildclass  extends ProtectedPrgm3rdWay 
+public class ProtectedChildclass extends ProtectedPrgm3rdWay 
 {
 
 	public static void main(String[] args) {
